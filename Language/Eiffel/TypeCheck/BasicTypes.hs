5b8c2e59dff624d289ed22c8adfbb8ffe25c1f5d
Scott West <scott.gregory.west@gmail.com>
2012-08-10 15:52:05 +0200

    Making numerical casts more liberal.
    This insn't my preferred solution, but I dont' think the Eiffel language
    really plays by its own rules.

--- Language/Eiffel/TypeCheck/BasicTypes.hs
+++ Language/Eiffel/TypeCheck/BasicTypes.hs
@@ -32,6 +32,10 @@ numericCanBe e t
     (t == (ClassType "REAL_64" []) || t == (ClassType "REAL_32" [])) = True
   | T.texprTyp e == (ClassType "INTEGER_32" []) &&
     t == (ClassType "INTEGER_64" []) = True
+  | isNaturalType (T.texprTyp e) && --  == ClassType "NATURAL_8" [] &&
+    (t == ClassType "INTEGER_32" [] || t == ClassType "NATURAL_32" []) = True
+  | isIntegerType (T.texprTyp e) &&
+    (t == ClassType "INTEGER_32" [] || t == ClassType "NATURAL_32" []) = True
   | otherwise = False
 
 guardTypePred :: (Typ -> Bool) -> String -> Typ -> TypingBody body Typ
